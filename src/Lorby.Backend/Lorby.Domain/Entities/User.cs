9b23d6083e491afacd9e8861575563d97d7c4080
isxoqovxasanboy <isxoqovxasanboy1@gmail.com>
2024-03-10 11:03:12 +0500

    Change User entity

--- src/Lorby.Backend/Lorby.Domain/Entities/User.cs
+++ src/Lorby.Backend/Lorby.Domain/Entities/User.cs
@@ -5,25 +5,39 @@ namespace Lorby.Domain.Entities;
 /// <summary>
 /// represents a user in the system.
 /// </summary>
+
+/// <summary>
+/// represents a user in the system.
+/// </summary>
 public class User : Entity
 {
     /// <summary>
     /// Gets or sets the full name of the user.
     /// </summary>
-    public string FullName { get; set; } = default!;
+    public string FirstName { get; set; } = default!;
+
+    /// <summary>
+    /// Gets or sets the last name of the user.
+    /// </summary>
+    public string LastName { get; set; } = default!;
+
+    /// <summary>
+    /// Gets the full name of the user.
+    /// </summary>
+    public string FullName => $"{FirstName} {LastName}";
 
     /// <summary>
     /// Gets or sets the email address of the user.
     /// </summary>
-    public string Email { get; set; } = default!;
+    public string EmailAddress { get; set; } = default!;
 
     /// <summary>
     /// Gets or sets the password of the user.
     /// </summary>
-    public string Password { get; set; } = default!;
+    public string PasswordHash { get; set; } = default!;
 
     /// <summary>
     /// Indicates whether the email address of the user has been verified.
     /// </summary>
-    public bool IsEmailVerified { get; set; }
+    public bool IsEmailAddressVerified { get; set; }
 }
