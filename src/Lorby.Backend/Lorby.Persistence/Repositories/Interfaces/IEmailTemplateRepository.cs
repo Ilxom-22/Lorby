0e778fcce52625a9884ae13364d3b49440071dc8
isxoqovxasanboy <isxoqovxasanboy1@gmail.com>
2024-03-10 12:53:44 +0500

    Add email template repository

---
+++ src/Lorby.Backend/Lorby.Persistence/Repositories/Interfaces/IEmailTemplateRepository.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq.Expressions;
+using Lorby.Domain.Entities;
+
+namespace Lorby.Persistence.Repositories.Interfaces;
+
+/// <summary>
+/// Represents a repository for managing email templates in the data store.
+/// </summary>
+public interface IEmailTemplateRepository
+{
+    /// <summary>
+    /// Gets a queryable collection of email templates based on the provided predicate.
+    /// </summary>
+    /// <param name="predicate">The predicate for filtering email templates (optional).</param>
+    /// <param name="asNoTracking">Flag indicating whether to use query tracking or not.</param>
+    /// <returns>An <see cref="IQueryable"/> of email templates.</returns>
+    IQueryable<EmailTemplate> Get(Expression<Func<EmailTemplate, bool>>? predicate = default, bool asNoTracking = false);
+}
