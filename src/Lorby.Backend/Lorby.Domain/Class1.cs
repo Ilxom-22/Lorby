3502f0aeab04f8c0aa0c2ba35319da44cbd735fd
isxoqovxasanboy <isxoqovxasanboy1@gmail.com>
2024-03-10 10:40:02 +0500

    Add AppDbContext

--- src/Lorby.Backend/Lorby.Domain/Class1.cs
+++
@@ -1,5 +0,0 @@
-﻿namespace Lorby.Domain;
-
-public class Class1
-{
-}
---
+++ src/Lorby.Backend/Lorby.Persistence/DataContext/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Lorby.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Lorby.Persistence.DataContext;
+
+/// <summary>
+/// Represents Database Context for the entire web application
+/// </summary>
+public class AppDbContext(DbContextOptions<AppDbContext> dbContext) : DbContext(dbContext)
+{
+    DbSet<User> Users => Set<User>(); 
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+    }
+}
+
